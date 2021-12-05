7033354e99a290da0f2b100a89deefbee5c973e9
juniorcfaj <46963611+juniorcfaj@users.noreply.github.com>
2021-12-05 02:16:48 -0300

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,7 @@
-from locust import HttpUser, task, TaskSet, HttpLocust
+from locust import HttpUser, task
 
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/hello")
         self.client.get("/world")
-
-class MyLocust(HttpLocust):
-    task_set = UserBehavior
-    
-    min_wait = 1000
-    max_wait = 4000
-    
-    host = http://localhost:8000
